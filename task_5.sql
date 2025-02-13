4cea2e791d74a45e3538a5921945443f3d3fc753
petergodwin <chinegbomkpapeter@gmail.com>
2025-02-13 23:02:33 +0100

    Populating Your Database with Your Very First Data

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- inserting a single row into 'Customer' table in 'alx_book_store'
-INSERT INTO Customers (customer_id, customer_name, email, address);
+INSERT INTO customer (customer_id, customer_name, email, address);
 VALUEs (1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave.)
